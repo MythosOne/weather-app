42ba04dffa91ae1b075bedfdf85ce8069e4d13ed
MythosOne <107143884+MythosOne@users.noreply.github.com>
2023-10-31 22:28:36 +0200

    Correct apiService, get axios work.

--- src/Api/apiService.js
+++ src/Api/apiService.js
@@ -4,7 +4,7 @@ const API_KEY = 'e59a1482d0be237c1680d29670faf7ca';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
 export async function getWeatherData(){
-    const resp = await axios.get(`${BASE_URL}/weather?q=Kharkiv&appid={${API_KEY}}&units=metric`)
+    const resp = await axios.get(`${BASE_URL}/weather?q=Kharkiv&appid=${API_KEY}&units=metric`)
 
     return resp.data
 }
